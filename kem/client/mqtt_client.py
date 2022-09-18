469f43fc27fab249cc7e0adabb24ebd6fbd0aaf8
Raven Townsend <rto45@uclive.ac.nz>
2022-09-18 14:14:31 +1200

    Continue update where hkdf and hmac libraries were integrated

--- kem/client/mqtt_client.py
+++ kem/client/mqtt_client.py
@@ -1,12 +1,22 @@
+import hashlib
+import random
+
 import select
 
 import client_config
-import security_level
 import validate
 import json
+import hmac
+from pqcrypto.kem.kyber512 import generate_keypair, encrypt, decrypt
+from hkdf.hkdf import hkdf_expand, hkdf_extract
 
-from custom_errors import InvalidParameterError
-from util import remaining_length_bytes, get_remaining_length_int
+import kem.security_level as security_level
+from kem.packet_types import *
+from kem.custom_errors import InvalidParameterError
+from kem.util import remaining_length_bytes, get_remaining_length_int
+
+KEY_LEN = 256
+
 
 
 class MqttClient:
@@ -25,12 +35,95 @@ class MqttClient:
             self.config = client_config.Config(client_id, input_ip, input_port, broker_ip, broker_port, cert, post_quantum_cert)
         self.security_level = security_level.NONE
         self.socket_list = []
+        self.rand_c = random.getrandbits(256)
+        self.pub_key, self.secret_key = generate_keypair()
+        ES = hkdf_extract(None, 0)
+        self.dES = hkdf_extract(ES, "derived")
+        # below variables will be set when required info is received from server
+        self.rand_s = None
+        self.shared_secret = None
+        self.dHS = None
+        self.client_hello = None
+        self.server_hello = None
+        self.client_kem_ciphtertext = None
+        self.client_finished = None
+
+
+    def kemtls_client_hello(self):
+        protocol_name = [ord('K'), ord('E'), ord('M'), ord('T'), ord('L'), ord('S')]
+        packet_type = [KEMTLS_CLIENT_HELLO]
+        self.client_hello = bytearray(protocol_name + packet_type + [self.rand_c, self.pub_key])
+        self.config.sock.sendall(self.client_hello)
+
+        # receive Server Hello
+        self.server_hello = self.config.sock.recv(4096)
+        self.handle_server_hello()
+
+    def handle_server_hello(self):
+        protocol_name = self.server_hello[0:6]
+        if protocol_name[0] != ord('K') \
+                or protocol_name[1] != ord('E') \
+                or protocol_name[2] != ord('M') \
+                or protocol_name[3] != ord('T') \
+                or protocol_name[4] != ord('L') \
+                or protocol_name[5] != ord('S'):
+            raise InvalidParameterError("Protocol name field is not equal to KEMTLS")
+
+        packet_type = self.server_hello[6]
+        if packet_type != KEMTLS_SERVER_HELLO:
+            raise InvalidParameterError("Packet type should be SERVER HELLO")
+
+        self.rand_s = int.from_bytes(self.server_hello[7:39], "big")  # read the next 32 bytes (256 bits) into rand_s
+        cipher_text_ephemeral = int.from_bytes(self.server_hello[39:], "big")  # read the remaining bits into cte
+        self.shared_secret = decrypt(self.secret_key, cipher_text_ephemeral)
+
+        HS = hkdf_extract(self.shared_secret, self.dES)
+        # CHTS = hkdf_expand(HS, "c hs traffic", KEY_LEN)
+        # SHTS = hkdf_expand(HS, "s hs traffic", KEY_LEN)
+        self.dHS = hkdf_expand(HS, "derived")
+
+        public_key_server = "ABC123"  # TODO extract this from certificate
+        cipher_text_s = encrypt(public_key_server, self.shared_secret)
+
+        protocol_name = [ord('K'), ord('E'), ord('M'), ord('T'), ord('L'), ord('S')]
+        packet_type = [KEMTLS_CLIENT_KEM_CIPHERTEXT]
+        self.client_kem_ciphtertext = bytearray(protocol_name + packet_type + [cipher_text_s])
+        self.config.sock.sendall(self.client_kem_ciphtertext)
+
+        AHS = hkdf_extract(self.shared_secret, self.dHS)
+        # CAHTS = hkdf_expand("c ahs traffic", AHS, KEY_LEN)
+        # SAHTS = hkdf_expand("s ahs traffic", AHS, KEY_LEN)
+        dAHS = hkdf_expand("derived", AHS)
+        MS = hkdf_extract(None, dAHS)
+        fk_c = hkdf_expand("c finished", MS, KEY_LEN)
+        fk_s = hkdf_expand("s finished", MS, KEY_LEN)
+
+        hmac_msg = self.client_hello + self.server_hello + self.client_kem_ciphtertext
+        hmac_obj = hmac.new(fk_c, hmac_msg, hashlib.sha3_256)
+
+        packet_type = [KEMTLS_CLIENT_FINISHED]
+        self.client_finished = bytearray(protocol_name + packet_type) + bytearray.fromhex(hmac_obj.hexdigest())
+        self.config.sock.sendall(self.client_finished)
+
+        # CATS = hkdf_expand("c ap traffic", MS, KEY_LEN)
+
+        server_finished = self.config.sock.recv(4096)
+        server_hmac = server_finished[7:]
+
+        hmac_msg += self.client_finished
+        own_hmac = hmac.new(fk_s, hmac_msg, hashlib.sha3_256)
+        hmacs_equal = hmac.compare_digest(own_hmac.digest(), server_hmac)
+
+        if not hmacs_equal:
+            raise Exception("HMACs not equal, something went wrong")
+
+
 
     def connect(self):
         """Sends a connect message. Returns the size of the packet"""
 
         # variable header
-        protocol_name = [0x00, 0x04, 0x4D, 0x51, 0x54, 0x54]
+        protocol_name = [0x00, 0x04, 0x4D, 0x51, 0x54, 0x54]  # [_, _, M, Q, T, T]
         protocol_level = [0x05]
         connect_flags = [0x02]
         keep_alive = [0x00, 0x00]
